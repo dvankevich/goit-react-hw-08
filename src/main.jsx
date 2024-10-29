feee5b814cb31dff2f59d9a816eafd4a83d19bd3
dvankevich <dvankevich@gmail.com>
2024-10-29 18:08:17 +0200

    add css modules

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './components/App'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import 'the-new-css-reset/css/reset.css';
+import 'modern-normalize';
+import App from './components/App';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
