dc9a8a7dd592815f104a52c5af1e76a821794f39
dima <dvankevich@gmail.com>
2024-10-31 18:01:01 +0200

    add Contact styles

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,18 +1,27 @@
 import { FaUserSecret } from 'react-icons/fa6';
 import { GiRotaryPhone } from 'react-icons/gi';
 import { TiDelete } from 'react-icons/ti';
+import s from './Contact.module.css';
 
 const Contact = ({ contact, deleteContact }) => {
   return (
-    <li>
-      <div>
-        <FaUserSecret />
-        <p>{contact.name}</p>
-        <GiRotaryPhone />
-        <p>{contact.number}</p>
+    <li className={s.contactCard}>
+      <div className={s.nameNumber}>
+        <div className={s.contactField}>
+          <FaUserSecret className={s.icon} />
+          <p>{contact.name}</p>
+        </div>
+        <div className={s.contactField}>
+          <GiRotaryPhone className={s.icon} />
+          <p>{contact.number}</p>
+        </div>
       </div>
-      <button type="button" onClick={() => deleteContact(contact.id)}>
-        <TiDelete />
+      <button
+        className={s.btn}
+        type="button"
+        onClick={() => deleteContact(contact.id)}
+      >
+        <TiDelete className={s.icon} />
         Delete
       </button>
     </li>
