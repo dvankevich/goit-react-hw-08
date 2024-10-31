1c7e9de361e8a43c89f0a2fabbbed72cbaf7adb0
dima <dvankevich@gmail.com>
2024-10-31 16:17:39 +0200

    add icons

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,18 @@
+import { FaUserSecret } from 'react-icons/fa6';
+import { GiRotaryPhone } from 'react-icons/gi';
+import { TiDelete } from 'react-icons/ti';
+
 const Contact = ({ contact, deleteContact }) => {
   return (
     <li>
       <div>
+        <FaUserSecret />
         <p>{contact.name}</p>
+        <GiRotaryPhone />
         <p>{contact.number}</p>
       </div>
       <button type="button" onClick={() => deleteContact(contact.id)}>
+        <TiDelete />
         Delete
       </button>
     </li>
