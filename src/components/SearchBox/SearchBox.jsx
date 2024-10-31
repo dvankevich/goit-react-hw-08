c49025212aac9fbd2d60b0a5ee4945c57d87e68a
Dmitry <dvankevich@gmail.com>
2024-10-31 19:33:38 +0200

    add SearchBox styles

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,11 +1,12 @@
 import { useId } from 'react';
+import s from "./SearchBox.module.css"
 
 const SearchBox = ({ search, handleSearch }) => {
   const searchInputId = useId();
   return (
-    <div>
+    <div className={s.box}>
       <label htmlFor={searchInputId}>Find contacts by name</label>
-      <input
+      <input className={s.field}
         id={searchInputId}
         onChange={evt => handleSearch(evt.target.value)}
         value={search}
