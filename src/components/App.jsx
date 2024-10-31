ae2918526ca2a2a4847a118b3f0e956de74d5130
dima <dvankevich@gmail.com>
2024-10-31 16:05:21 +0200

    add local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,23 @@
 import './App.css';
 import contactsInit from '../contacts.json';
 import ContactList from './ContactList/ContactList';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import SearchBox from './SearchBox/SearchBox';
 import ContactForm from './ContactForm/ContactForm';
 
 function App() {
-  const [contacts, setContacts] = useState(contactsInit);
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = window.localStorage.getItem('saved-contacts');
+    if (savedContacts !== null) {
+      return JSON.parse(savedContacts);
+    }
+    return contactsInit;
+  });
   const [searchStr, setSearchStr] = useState('');
+
+  useEffect(() => {
+    window.localStorage.setItem('saved-contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const deleteContact = id => {
     setContacts(prevContactList =>
