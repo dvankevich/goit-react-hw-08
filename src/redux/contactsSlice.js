7095fec8a8a42e654682cdffc821f6e7ca9309c6
dima <dvankevich@gmail.com>
2024-11-19 12:25:11 +0200

    add temporary initialState values

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -5,7 +5,10 @@ const slice = createSlice({
   name: "contacts",
   // Початковий стан редюсера слайсу
   initialState: {
-    items: [],
+      items: [{ "id": "id-1", "name": "Rosie Simpson_", "number": "459-12-56" },
+          { "id": "id-2", "name": "Hermione Kline_", "number": "443-89-12" },
+          { "id": "id-3", "name": "Eden Clements_", "number": "645-17-79" },
+          { "id": "id-4", "name": "Annie Copeland_", "number": "227-91-26" }],
   },
   // Об'єкт case-редюсерів
   reducers: {
