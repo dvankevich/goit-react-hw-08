e67157f6a3ea819ae2cd5ab5778bf0896b7fdecb
dvankevich <dvankevich@gmail.com>
2024-11-19 20:04:25 +0200

    modify SearchBox

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,15 +1,22 @@
 import { useId } from 'react';
-import s from "./SearchBox.module.css"
+import s from './SearchBox.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { changeFilter } from '../../redux/filtersSlice';
 
-const SearchBox = ({ search, handleSearch }) => {
+const SearchBox = () => {
   const searchInputId = useId();
+  const dispatch = useDispatch();
+  const filter = useSelector(state => state.filters.name);
+  const handleSearch = filter => dispatch(changeFilter(filter));
+
   return (
     <div className={s.box}>
       <label htmlFor={searchInputId}>Find contacts by name</label>
-      <input className={s.field}
+      <input
+        className={s.field}
         id={searchInputId}
         onChange={evt => handleSearch(evt.target.value)}
-        value={search}
+        value={filter}
       ></input>
     </div>
   );
