71a061d1a1ebf0dce2893fd7d795141b22cb11f1
dima <dvankevich@gmail.com>
2024-11-20 09:32:14 +0200

    add rootReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,19 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import contactsReducer from "./contactsSlice"
 import filtersReducer from "./filtersSlice";
 
+// export const store = configureStore({
+//   reducer: {
+//     contacts: contactsReducer,
+//     filters: filtersReducer,
+//   },
+// });
+
+const rootReducer = combineReducers({ 
+  contacts: contactsReducer,
+  filters: filtersReducer,
+})
+
 export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-    filters: filtersReducer,
-  },
-});
+  reducer: rootReducer
+})
