c6994e20c01aabf5737ab9fe52581b0fe34093a0
dvankevich <dvankevich@gmail.com>
2024-11-21 17:41:13 +0200

    add createAsyncThunk function

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -14,10 +14,27 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 // для всіх запитів axios
 axios.defaults.baseURL = 'https://673f006da9bc276ec4b6b6b0.mockapi.io/';
 
-export const fetchContacts = createAsyncThunk('tasks/fetchAll', async () => {
-  const response = await axios.get('/contacts');
-  return response.data;
-});
+// export const fetchContacts = createAsyncThunk('tasks/fetchAll', async () => {
+//   const response = await axios.get('/contacts');
+//   return response.data;
+// });
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  // Використовуємо символ підкреслення як ім'я першого параметра,
+  // тому що в цій операції він нам не потрібен
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/contacts');
+      // При успішному запиті повертаємо проміс із даними
+      return response.data;
+    } catch (e) {
+      // При помилці запиту повертаємо проміс
+      // який буде відхилений з текстом помилки
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
 
 // // Оголошення асинхронної операції
 // // fetchContacts для отримання даних
