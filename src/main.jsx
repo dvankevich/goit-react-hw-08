1a520615e7122c54724e7dbbb27eb3f7880bdc4c
dvankevich <dvankevich@gmail.com>
2024-11-22 09:26:50 +0200

    remove PersistGate

--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,13 @@ import './index.css';
 // 1. Імпортуємо провайдер
 import { Provider } from 'react-redux';
 // 2. Імпортуємо створений раніше стор
-import { store, persistor } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from './redux/store';
+//import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,51 +1,51 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import contactsReducer from "./contactsSlice"
-import filtersReducer from "./filtersSlice";
-import storage from "redux-persist/lib/storage";
-import persistReducer from "redux-persist/lib/persistReducer";
-import persistStore from "redux-persist/lib/persistStore";
-import {
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist'
+import { configureStore } from '@reduxjs/toolkit';
+import contactsReducer from './contactsSlice';
+import filtersReducer from './filtersSlice';
+// import storage from "redux-persist/lib/storage";
+// import persistReducer from "redux-persist/lib/persistReducer";
+// import persistStore from "redux-persist/lib/persistStore";
+// import {
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist'
 
 //https://blog.logrocket.com/persist-state-redux-persist-redux-toolkit-react/
 
-// export const store = configureStore({
-//   reducer: {
-//     contacts: contactsReducer,
-//     filters: filtersReducer,
-//   },
-// });
-
-const persistConfig = {
-  key: "root",
-  storage,
-  blacklist:['filters'], // не зберігати filters
-  
-};
-
-const rootReducer = combineReducers({ 
-  contacts: contactsReducer,
-  filters: filtersReducer,
-})
-
-const persistedReducer = persistReducer(persistConfig, rootReducer)
-
 export const store = configureStore({
-  reducer: persistedReducer,
-  // hide A non-serializable value was detected in an action, in the path: `register` error
-  // https://redux-toolkit.js.org/usage/usage-guide#use-with-redux-persist
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-})
+  reducer: {
+    contacts: contactsReducer,
+    filters: filtersReducer,
+  },
+});
 
-export const persistor = persistStore(store);
+// const persistConfig = {
+//   key: "root",
+//   storage,
+//   blacklist:['filters'], // не зберігати filters
+
+// };
+
+// const rootReducer = combineReducers({
+//   contacts: contactsReducer,
+//   filters: filtersReducer,
+// })
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+// export const store = configureStore({
+//   reducer: persistedReducer,
+//   // hide A non-serializable value was detected in an action, in the path: `register` error
+//   // https://redux-toolkit.js.org/usage/usage-guide#use-with-redux-persist
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }),
+// })
+
+// export const persistor = persistStore(store);
