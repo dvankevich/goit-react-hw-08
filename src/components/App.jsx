4711b696a0650a9b34997ada87218a8bc303d4ae
dvankevich <dvankevich@gmail.com>
2024-11-25 18:42:49 +0200

    add restricted and private routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,62 @@
 import './App.css';
 
-import ContactList from './ContactList/ContactList';
+//import ContactList from './ContactList/ContactList';
 import { useEffect } from 'react';
-import SearchBox from './SearchBox/SearchBox';
-import ContactForm from './ContactForm/ContactForm';
+//import SearchBox from './SearchBox/SearchBox';
+//import ContactForm from './ContactForm/ContactForm';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchContacts } from '../redux/contacts/operations';
+import { refreshUser } from '../redux/auth/operations';
+import { selectIsRefreshing } from '../redux/auth/selectors';
+import { Layout } from './Layout';
+import { Route, Routes } from 'react-router-dom';
+import { PrivateRoute } from './PrivateRoute';
+import { RestrictedRoute } from './RestrictedRoute';
+import HomePage from '../pages/HomePage/HomePage';
+import RegisterPage from '../pages/RegisterPage/RegisterPage';
+import LoginPage from '../pages/LoginPage/LoginPage';
+import TasksPage from '../pages/TasksPage/TasksPage';
+
+//import { fetchContacts } from '../redux/contacts/operations';
 // Імпортуємо функції-селектори
-import { selectIsLoading, selectError } from '../redux/selectors';
+// import { selectIsLoading, selectError } from '../redux/selectors';
 
 function App() {
   const dispatch = useDispatch();
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
+  const isRefreshing = useSelector(selectIsRefreshing);
+  //const isLoading = useSelector(selectIsLoading);
+  //const error = useSelector(selectError);
 
   useEffect(() => {
-    dispatch(fetchContacts());
+    //dispatch(fetchContacts());
+    dispatch(refreshUser());
   }, [dispatch]);
 
-  return (
-    <>
-      <h1>Phonebook</h1>
-      <div>
-        {isLoading && !error && <p>Loading contacts...</p>}
-        {error && <p>{error}</p>}
-      </div>
-      <ContactForm />
-      <SearchBox />
-      <ContactList />
-    </>
+  return isRefreshing ? (
+    <b>Refreshing user...</b>
+  ) : (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute redirectTo="/tasks" component={<RegisterPage />} />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/tasks" component={<LoginPage />} />
+          }
+        />
+        <Route
+          path="/tasks"
+          element={
+            <PrivateRoute redirectTo="/login" component={<TasksPage />} />
+          }
+        />
+      </Routes>
+    </Layout>
   );
 }
 
