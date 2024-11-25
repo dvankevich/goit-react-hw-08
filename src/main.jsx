2e1249f2b5e378728ae4ad75c52de5677898b0cf
dima <dvankevich@gmail.com>
2024-11-25 11:10:26 +0200

    add BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { HelmetProvider } from 'react-helmet-async';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import 'the-new-css-reset/css/reset.css';
 import 'modern-normalize';
 import App from './components/App';
@@ -13,9 +14,11 @@ import { store } from './redux/store';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
+      <BrowserRouter>
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
