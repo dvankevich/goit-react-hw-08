4b08db834a943bac2fda61c6f7ead634b04bde8c
dima <dvankevich@gmail.com>
2024-11-25 10:20:46 +0200

    move/rename files on redux/filters

---
+++ src/redux/contacts/operations.js
@@ -0,0 +1,51 @@
+// fetchContacts - одержання масиву контактів (метод GET) запитом. Базовий тип екшену це рядок "contacts/fetchAll".
+// addContact - додавання нового контакту (метод POST). Базовий тип екшену це рядок "contacts/addContact".
+// deleteContact - видалення контакту по ID (метод DELETE). Базовий тип екшену це рядок "contacts/deleteContact".
+
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+// Встановлюємо базову URL-адресу
+// для всіх запитів axios
+axios.defaults.baseURL = 'https://673f006da9bc276ec4b6b6b0.mockapi.io/';
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  // Використовуємо символ підкреслення як ім'я першого параметра,
+  // тому що в цій операції він нам не потрібен
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/contacts');
+      // При успішному запиті повертаємо проміс із даними
+      return response.data;
+    } catch (e) {
+      // При помилці запиту повертаємо проміс
+      // який буде відхилений з текстом помилки
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  'contact/addContact',
+  async ({ name, number }, thunkAPI) => {
+    try {
+      const response = await axios.post('/contacts', { name, number });
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  'contact/deleteContact',
+  async (taskId, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/contacts/${taskId}`);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
