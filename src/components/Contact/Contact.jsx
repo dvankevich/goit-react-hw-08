5415115c90a09bb552c75fad5ac88eb4677847bd
dvankevich <dvankevich@gmail.com>
2024-11-26 19:42:37 +0200

    add confirmAlert

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,10 +4,31 @@ import { TiDelete } from 'react-icons/ti';
 import s from './Contact.module.css';
 import { deleteContact } from '../../redux/contacts/operations';
 import { useDispatch } from 'react-redux';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
 const Contact = ({ contact }) => {
   const dispatch = useDispatch();
   const handleDelete = () => dispatch(deleteContact(contact.id));
+
+  const submit = () => {
+    confirmAlert({
+      title: 'Confirm to delete',
+      message: 'Are you sure to do this?',
+      buttons: [
+        {
+          label: 'Yes',
+          onClick: () => handleDelete(),
+        },
+        {
+          label: 'No',
+          // https://quickref.me/create-an-empty-function.html
+          onClick: () => {},
+        },
+      ],
+    });
+  };
+
   return (
     <li className={s.contactCard}>
       <div className={s.nameNumber}>
@@ -20,7 +41,8 @@ const Contact = ({ contact }) => {
           <p>{contact.number}</p>
         </div>
       </div>
-      <button className={s.btn} type="button" onClick={() => handleDelete()}>
+      {/* <button className={s.btn} type="button" onClick={() => handleDelete()}> */}
+      <button className={s.btn} type="button" onClick={() => submit()}>
         <TiDelete className={s.icon} />
         Delete
       </button>
