a98addc4b4f4215a78ce7285f0562f4eb5b69ca4
dima <dvankevich@gmail.com>
2024-12-05 09:55:04 +0200

    remove comments

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,13 +4,10 @@ import { GiRotaryPhone } from 'react-icons/gi';
 import { deleteContact, editContact } from '../../redux/contacts/operations';
 import { useDispatch } from 'react-redux';
 //import { confirmAlert } from 'react-confirm-alert';
-import 'react-confirm-alert/src/react-confirm-alert.css';
+//import 'react-confirm-alert/src/react-confirm-alert.css';
 import s from './Contact.module.css';
-import './react-confirm-alert.css';
+//import './react-confirm-alert.css';
 
-//import { ErrorMessage, Field, Form, Formik } from 'formik';
-//import { nanoid } from 'nanoid';
-//import * as Yup from 'yup';
 import { Button } from '@mui/material';
 import { Delete } from '@mui/icons-material';
 //import { Delete, Edit } from '@mui/icons-material';
@@ -20,22 +17,7 @@ import ModalForm from '../ModalForm/ModalForm';
 const Contact = ({ contact }) => {
   const dispatch = useDispatch();
   const handleDelete = () => dispatch(deleteContact(contact.id));
-  // const handleEdit = (values, actions) => {
-  //   dispatch(
-  //     editContact({
-  //       id: values.id,
-  //       name: values.name,
-  //       number: values.number,
-  //     })
-  //   );
-  //   actions.resetForm({
-  //     values: {
-  //       id: values.id,
-  //       name: values.name,
-  //       number: values.number,
-  //     },
-  //   });
-  // };
+
   const handleEdit = values => {
     dispatch(
       editContact({
@@ -45,91 +27,6 @@ const Contact = ({ contact }) => {
       })
     );
   };
-  // const updateDialog = () => {
-  //   const initialValues = {
-  //     id: contact.id,
-  //     name: contact.name,
-  //     number: contact.number,
-  //   };
-
-  //   const FeedbackSchema = Yup.object().shape({
-  //     name: Yup.string()
-  //       .min(2, 'Name is too Short!')
-  //       .max(50, 'Name is too Long!')
-  //       .required('Required'),
-  //     number: Yup.string()
-  //       .matches(
-  //         /^(?:\+38)?(\(0\d{2}\)\d{3}-\d{4}|\(0\d{2}\)\d{3}-\d{2}-\d{2}|0\d{9}|\d{3}-\d{2}-\d{2})$/,
-  //         'Incorrect phone number format'
-  //       ) // Регулярний вираз для валідації from https://poe.com/s/e3yJBmpLrsdnOqOGdjrQ
-  //       .required('Required!')
-  //       .typeError('Enter phone-number!'),
-  //   });
-  //   const nameFieldId = nanoid();
-  //   const numberFieldId = nanoid();
-
-  //   confirmAlert({
-  //     title: 'Confirm to update',
-  //     message: 'Are you sure to do this?',
-  //     childrenElement: () => {
-  //       return (
-  //         <div>
-  //           <p>Custom UI</p>
-  //           <Formik
-  //             initialValues={initialValues}
-  //             onSubmit={handleEdit}
-  //             validationSchema={FeedbackSchema}
-  //             // resetForm
-  //           >
-  //             <Form className={s.form}>
-  //               <label htmlFor={nameFieldId}>Name</label>
-  //               <Field
-  //                 className={s.field}
-  //                 type="text"
-  //                 name="name"
-  //                 id={nameFieldId}
-  //               />
-  //               <ErrorMessage
-  //                 name="name"
-  //                 component="span"
-  //                 className={s.error}
-  //               />
-
-  //               <label htmlFor={numberFieldId}>Number</label>
-  //               <Field
-  //                 className={s.field}
-  //                 type="text"
-  //                 name="number"
-  //                 id={numberFieldId}
-  //                 placeholder="XXX-XX-XX"
-  //               />
-  //               <ErrorMessage
-  //                 name="number"
-  //                 component="span"
-  //                 className={s.error}
-  //               />
-
-  //               <button className={s.btn} type="submit">
-  //                 Update contact
-  //               </button>
-  //             </Form>
-  //           </Formik>
-  //         </div>
-  //       );
-  //     },
-  //     buttons: [
-  //       // {
-  //       //   label: 'Yes',
-  //       //   onClick: () => handleDelete(),
-  //       // },
-  //       {
-  //         label: 'Close',
-  //         // https://quickref.me/create-an-empty-function.html
-  //         onClick: () => {},
-  //       },
-  //     ],
-  //   });
-  // };
 
   return (
     <li className={s.contactCard}>
