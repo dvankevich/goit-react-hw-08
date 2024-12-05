44327943f122c3bf0f99a3c3d575a590c425aa42
dvankevich <dvankevich@gmail.com>
2024-12-05 21:17:11 +0200

    add MUI to LoginForm

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,8 +1,8 @@
 import { useDispatch } from 'react-redux';
 import { logIn } from '../../redux/auth/operations';
 import toast from 'react-hot-toast';
-import css from './LoginForm.module.css';
-import { Button } from '@mui/material';
+//import css from './LoginForm.module.css';
+import { Box, Button, TextField } from '@mui/material';
 
 export const LoginForm = () => {
   const dispatch = useDispatch();
@@ -31,19 +31,49 @@ export const LoginForm = () => {
   };
 
   return (
-    <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
-      <label className={css.label}>
-        Email
-        <input type="email" name="email" />
-      </label>
-      <label className={css.label}>
-        Password
-        <input type="password" name="password" />
-      </label>
-      {/* <button type="submit">Log In</button> */}
-      <Button type="submit" variant="contained">
+    // <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
+    //   <label className={css.label}>
+    //     Email
+    //     <input type="email" name="email" />
+    //   </label>
+    //   <label className={css.label}>
+    //     Password
+    //     <input type="password" name="password" />
+    //   </label>
+    //   {/* <button type="submit">Log In</button> */}
+    //   <Button type="submit" variant="contained">
+    //     Log In
+    //   </Button>
+    // </form>
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      autoComplete="off"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 2,
+        maxWidth: 400,
+        margin: 'auto',
+      }}
+    >
+      <TextField
+        label="Email"
+        type="email"
+        name="email"
+        variant="outlined"
+        fullWidth
+      />
+      <TextField
+        label="Password"
+        type="password"
+        name="password"
+        variant="outlined"
+        fullWidth
+      />
+      <Button type="submit" variant="contained" color="primary">
         Log In
       </Button>
-    </form>
+    </Box>
   );
 };
