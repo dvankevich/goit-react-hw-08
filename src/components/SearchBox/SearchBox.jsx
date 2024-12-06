e34ff37de8289cd94d4194584ee41e953cfbeaef
dima <dvankevich@gmail.com>
2024-12-06 11:00:09 +0200

    add MUI to SearchBox

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,7 +1,9 @@
 import { useId } from 'react';
-import s from './SearchBox.module.css';
+//import s from './SearchBox.module.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { changeFilter } from '../../redux/filters/slice';
+import { Box, TextField } from '@mui/material';
+import { Search } from '@mui/icons-material';
 
 const SearchBox = () => {
   const searchInputId = useId();
@@ -10,15 +12,27 @@ const SearchBox = () => {
   const handleSearch = filter => dispatch(changeFilter(filter));
 
   return (
-    <div className={s.box}>
-      <label htmlFor={searchInputId}>Find contacts by name</label>
-      <input
-        className={s.field}
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: '8px',
+        marginTop: '20px',
+        width: '100%',
+      }}
+    >
+      <TextField
         id={searchInputId}
+        variant="outlined"
         onChange={evt => handleSearch(evt.target.value)}
         value={filter}
-      ></input>
-    </div>
+        fullWidth={false}
+        sx={{ width: '60%' }}
+        placeholder="Search contacts by Name or number ..."
+      />
+      <Search />
+    </Box>
   );
 };
 
