cd3f9b605d93e42e56f842d2a76b37535cf344b5
dima <dvankevich@gmail.com>
2024-12-06 09:34:29 +0200

    add MUI to Contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,14 +1,17 @@
-import { FaUserSecret } from 'react-icons/fa6';
-import { GiRotaryPhone } from 'react-icons/gi';
+// import { FaUserSecret } from 'react-icons/fa6';
+// import { GiRotaryPhone } from 'react-icons/gi';
+
+import PersonIcon from '@mui/icons-material/Person';
+import PhoneIcon from '@mui/icons-material/Phone';
 
 import { deleteContact, editContact } from '../../redux/contacts/operations';
 import { useDispatch } from 'react-redux';
 //import { confirmAlert } from 'react-confirm-alert';
 //import 'react-confirm-alert/src/react-confirm-alert.css';
-import s from './Contact.module.css';
+//import s from './Contact.module.css';
 //import './react-confirm-alert.css';
 
-import { Button } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import { Delete } from '@mui/icons-material';
 //import { Delete, Edit } from '@mui/icons-material';
 import ConfirmationDialog from '../ConfirmationDialog/ConfirmationDialog';
@@ -29,58 +32,118 @@ const Contact = ({ contact }) => {
   };
 
   return (
-    <li className={s.contactCard}>
-      <div className={s.nameNumber}>
-        <div className={s.contactField}>
-          <FaUserSecret className={s.icon} />
-          <p>{contact.name}</p>
-        </div>
-        <div className={s.contactField}>
-          <GiRotaryPhone className={s.icon} />
-          <p>{contact.number}</p>
-        </div>
-      </div>
-      <div className={s.buttons}>
-        {/* <Button
-          variant="contained"
-          type="button"
-          onClick={updateDialog}
-          startIcon={<Edit />}
-        >
-          Edit
-        </Button> */}
-
-        <ModalForm
-          title="Edit contact"
-          initialValues={{
-            id: contact.id,
-            name: contact.name,
-            number: contact.number,
-          }}
-          // onSubmit={handleSubmit}
-          onSubmit={handleEdit}
-          openBtnLabel="Edit"
-          submitBtnLabel="Update"
-        />
-
-        <ConfirmationDialog
-          title="Contact delete"
-          description="Are you sure you want to proceed?"
-          response={handleDelete}
-        >
-          {showDialog => (
-            <Button
-              variant="contained"
-              type="button"
-              onClick={showDialog}
-              startIcon={<Delete />}
+    <li
+      style={{
+        listStyle: 'none',
+        marginBottom: '8px',
+      }}
+    >
+      <Box
+        sx={{
+          width: '300px',
+          padding: '16px',
+          border: '1px solid #ccc',
+          borderRadius: '8px',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '12px',
+        }}
+      >
+        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <Box
+            sx={{ display: 'flex', alignItems: 'center', marginBottom: '8px' }}
+          >
+            <PersonIcon style={{ marginRight: '8px' }} />
+            <Typography
+              variant="body1"
+              sx={{
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+                whiteSpace: 'nowrap',
+                maxWidth: '200px',
+              }}
             >
-              Delete
-            </Button>
-          )}
-        </ConfirmationDialog>
-      </div>
+              {contact.name}
+            </Typography>
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <PhoneIcon style={{ marginRight: '8px' }} />
+            <Typography variant="body1">{contact.number}</Typography>
+          </Box>
+        </Box>
+        <Box sx={{ display: 'flex', gap: '8px', flexDirection: 'column' }}>
+          <ModalForm
+            title="Edit contact"
+            initialValues={{
+              id: contact.id,
+              name: contact.name,
+              number: contact.number,
+            }}
+            onSubmit={handleEdit}
+            openBtnLabel="Edit"
+            submitBtnLabel="Update"
+          />
+          <ConfirmationDialog
+            title="Contact delete"
+            description="Are you sure you want to proceed?"
+            response={handleDelete}
+          >
+            {showDialog => (
+              <Button
+                variant="contained"
+                type="button"
+                onClick={showDialog}
+                startIcon={<Delete />}
+              >
+                Delete
+              </Button>
+            )}
+          </ConfirmationDialog>
+        </Box>
+      </Box>
     </li>
+    // <li className={s.contactCard}>
+    //   <div className={s.nameNumber}>
+    //     <div className={s.contactField}>
+    //       <FaUserSecret className={s.icon} />
+    //       <p>{contact.name}</p>
+    //     </div>
+    //     <div className={s.contactField}>
+    //       <GiRotaryPhone className={s.icon} />
+    //       <p>{contact.number}</p>
+    //     </div>
+    //   </div>
+    //   <div className={s.buttons}>
+    //     <ModalForm
+    //       title="Edit contact"
+    //       initialValues={{
+    //         id: contact.id,
+    //         name: contact.name,
+    //         number: contact.number,
+    //       }}
+    //       onSubmit={handleEdit}
+    //       openBtnLabel="Edit"
+    //       submitBtnLabel="Update"
+    //     />
+
+    //     <ConfirmationDialog
+    //       title="Contact delete"
+    //       description="Are you sure you want to proceed?"
+    //       response={handleDelete}
+    //     >
+    //       {showDialog => (
+    //         <Button
+    //           variant="contained"
+    //           type="button"
+    //           onClick={showDialog}
+    //           startIcon={<Delete />}
+    //         >
+    //           Delete
+    //         </Button>
+    //       )}
+    //     </ConfirmationDialog>
+    //   </div>
+    // </li>
   );
 };
 
